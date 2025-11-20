630161941892f6b8bcc489bd201da0b60524550c
Alina <alinaroshchina5@gmail.com>
2025-11-20 09:50:47 +0300

    Add files via upload

---
+++ LabWork/LabWork14/DatabaseLibrary/Models/Film.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatabaseLibrary.Models;
+
+public partial class Film
+{
+    public int FilmId { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public short Duration { get; set; }
+
+    public short ReleaseYear { get; set; }
+
+    public string? Description { get; set; }
+
+    public byte[]? Poster { get; set; }
+
+    public string? AgeRating { get; set; }
+
+    public DateOnly? RentalStart { get; set; }
+
+    public DateOnly? RentalEnd { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public string? Genre { get; set; }
+}
